6cabb1c68a3b6044f54d08ccda6c7385225e9a2f
amir <mail.hampacredit.com>
2023-07-03 10:47:15 +0330

    install store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {environment} from "../environments/environment";
+import {NgxsDispatchPluginModule} from "@ngxs-labs/dispatch-decorator";
+import {NgxsRouterPluginModule} from "@ngxs/router-plugin";
+import {NgxsModule} from "@ngxs/store";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
+    }),
+    NgxsDispatchPluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
