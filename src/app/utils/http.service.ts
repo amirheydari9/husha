5f9a56e75fc5846a66284dd61290d086a6284fc7
amir <mail.hampacredit.com>
2023-07-15 14:34:59 +0330

    add http util

---
+++ src/app/utils/http.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  get<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.get<T>(url, {params, headers})
+  }
+
+  post<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.post<T>(url, body, {params, headers})
+  }
+
+  put<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.put<T>(url, body, {params, headers})
+  }
+
+  patch<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.patch<T>(url, body, {params, headers})
+  }
+
+  delete<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.delete<T>(url, {params, headers})
+  }
+}
