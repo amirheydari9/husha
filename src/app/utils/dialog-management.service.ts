77da44a006277846188e2f32336322d629c148d7
amir <mail.hampacredit.com>
2023-10-11 15:53:04 +0330

    create dialog management service

---
+++ src/app/utils/dialog-management.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {DialogService, DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
+import {Observable, Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogManagementService {
+
+  constructor(
+    private dialogService: DialogService
+  ) {
+  }
+
+  dialogConfig: DynamicDialogConfig = {
+    maximizable: true,
+    rtl: true,
+    draggable: true,
+    resizable: true,
+    modal: true,
+    width: '50vw',
+    height: '50vh'
+  }
+
+  openDialog(component: any, options: DynamicDialogConfig): Observable<any> {
+    const config = {...this.dialogConfig, options}
+    const dialogClosedSubject: Subject<any> = new Subject<any>();
+    const ref: DynamicDialogRef = this.dialogService.open(component, config);
+    ref.onClose.subscribe(data => {
+      dialogClosedSubject.next(data);
+      dialogClosedSubject.complete();
+    });
+    return dialogClosedSubject.asObservable();
+  }
+}
