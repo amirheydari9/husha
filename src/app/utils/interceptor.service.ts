89b045937a0f675f2365fba83a2080443896312b
amir <mail.hampacredit.com>
2023-12-04 14:14:01 +0330

    handle cache

--- src/app/utils/interceptor.service.ts
+++ src/app/utils/interceptor.service.ts
@@ -7,7 +7,7 @@ import {
   HttpRequest,
   HttpResponse
 } from "@angular/common/http";
-import {catchError, filter, finalize, map, Observable, takeUntil, throwError} from "rxjs";
+import {catchError, filter, finalize, map, Observable, of, takeUntil, tap, throwError} from "rxjs";
 import {TokenStorageService} from "./token-storage.service";
 import {Router} from "@angular/router";
 import {AppConfigService} from "./app-config.service";
@@ -20,6 +20,8 @@ import {showNotification} from "../constants/keys";
 export class InterceptorService implements HttpInterceptor {
 
   TOKEN_HEADER_KEY = 'Authorization';
+  private _cache: Map<string, { expireDate: Date, response: HttpResponse<any> }> = new Map()
+
 
   constructor(
     private tokenStorageService: TokenStorageService,
@@ -33,43 +35,57 @@ export class InterceptorService implements HttpInterceptor {
     });
   }
 
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let request = req;
     if (!request.url.startsWith('/assets')) {
-      this.appConfigService.setLoading(true)
       request = request.clone({url: environment.baseUrl.concat(request.url)})
-      const token = this.tokenStorageService.getToken();
-      if (token) {
-        request = this.addTokenHeader(request, token)
+      const cacheResponse = this._cache.get(request.urlWithParams)
+      if (cacheResponse && cacheResponse.expireDate <= new Date()) {
+        this._cache.delete(request.urlWithParams)
+        return this._sendRequest(request, next)
       }
-      return next.handle(request).pipe(
-        takeUntil(this.appConfigService.onCancelPendingRequests()),
-        filter(res => res instanceof HttpResponse),
-        // map((res: HttpResponse<any>) => res.clone({body: res.body.response})),
-        map((res: HttpResponse<any>) => this.handleResponse(res, request)),
-        catchError(error => {
-          if (error instanceof HttpErrorResponse) this.errorHandler(error)
-          return throwError(error);
-        }),
-        finalize(() => this.appConfigService.setLoading(false))
-      )
+      return cacheResponse ? of(new HttpResponse<any>({body: cacheResponse.response})) : this._sendRequest(request, next);
     } else {
       return next.handle(request);
     }
+  }
+
+  private _sendRequest(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.appConfigService.setLoading(true)
+    const token = this.tokenStorageService.getToken();
+    if (token) {
+      request = this.addTokenHeader(request, token)
+    }
+    return next.handle(request).pipe(
+      takeUntil(this.appConfigService.onCancelPendingRequests()),
+      filter(res => res instanceof HttpResponse),
+      map((response: HttpResponse<any>) => this.handleResponse(request, response)),
+      catchError(error => {
+        if (error instanceof HttpErrorResponse) this.errorHandler(error)
+        return throwError(error);
+      }),
+      finalize(() => this.appConfigService.setLoading(false))
+    )
   }
 
   private addTokenHeader(request: HttpRequest<any>, token: string): HttpRequest<any> {
     return request.clone({headers: request.headers.set(this.TOKEN_HEADER_KEY, 'Bearer ' + token)});
   }
 
-  private handleResponse(res: HttpResponse<any>, req: HttpRequest<any>): HttpResponse<any> {
+  private handleResponse(req: HttpRequest<any>, res: HttpResponse<any>): HttpResponse<any> {
     if (res.body.error) {
       throw new HttpErrorResponse({status: 400, error: res.body.error})
     } else {
       if (['PUT', 'PATCH', 'DELETE'].indexOf(req.method) !== -1 || req.headers.get(showNotification)) {
         this.notificationService.success('موفق', 'عملیات موردنظر با موفقیت انجام شد')
       }
-      return res.clone({body: res.body.response});
+      const response = res.body.response
+      if (req.method === "GET") {
+        const expireDate = new Date(Date.now() + environment.cacheTimeForHttpRequest)
+        this._cache.set(req.urlWithParams, {expireDate, response})
+      }
+      return res.clone({body: response});
     }
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: '/',
-  basicAuthUsername:'HushaMicroService',
-  basicAuthPassword:'Jfbg&z3dHM:m_Vcb',
+  basicAuthUsername: 'HushaMicroService',
+  basicAuthPassword: 'Jfbg&z3dHM:m_Vcb',
+  cacheTimeForHttpRequest: 60 * 60 * 8 * 1000
 };
