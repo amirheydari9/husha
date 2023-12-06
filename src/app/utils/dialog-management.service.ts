307ed43f462654457dec4a7fbee6edf7707d0c94
amir <mail.hampacredit.com>
2023-12-06 16:40:46 +0330

    handle close dialog after routing

--- src/app/utils/dialog-management.service.ts
+++ src/app/utils/dialog-management.service.ts
@@ -1,15 +1,24 @@
 import {Injectable} from '@angular/core';
 import {DialogService, DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
 import {Observable, Subject} from "rxjs";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogManagementService {
 
+  ref: DynamicDialogRef
+
   constructor(
-    private dialogService: DialogService
+    private dialogService: DialogService,
+    private router: Router
   ) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.ref.close()
+      }
+    })
   }
 
   dialogConfig: DynamicDialogConfig = {
@@ -24,11 +33,12 @@ export class DialogManagementService {
     // closeOnEscape: false
   }
 
+
   openDialog(component: any, options?: DynamicDialogConfig): Observable<any> {
     const config = {...this.dialogConfig, ...options}
     const dialogClosedSubject: Subject<any> = new Subject<any>();
-    const ref: DynamicDialogRef = this.dialogService.open(component, config);
-    ref.onClose.subscribe(data => {
+    this.ref = this.dialogService.open(component, config);
+    this.ref.onClose.subscribe(data => {
       dialogClosedSubject.next(data);
       dialogClosedSubject.complete();
     });
