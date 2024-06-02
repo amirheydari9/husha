3b6467caad874ae4c4bfb78fadd223e693ac6bf2
amir <mail.hampacredit.com>
2024-06-02 14:29:19 +0330

    create request methode

--- src/app/utils/http.service.ts
+++ src/app/utils/http.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpEvent, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {noCacheHeader, showNotificationHeader} from "../constants/keys";
 
@@ -13,25 +13,54 @@ export class HttpService {
   ) {
   }
 
-  get<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
-    return this.http.get<T>(url, {params, headers})
+  request<T>(method: string, url: string, body?: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    const options = {
+      body: body,
+      params: params,
+      headers: headers,
+    };
+    return this.http.request<T>(method, url, options);
   }
+
+  get<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.request<T>('GET', url, null, params, headers);
+  }
+
+  // get<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+  //   return this.http.get<T>(url, {params, headers})
+  // }
 
   post<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
-    return this.http.post<T>(url, body, {params, headers})
+    return this.request<T>('POST', url, body, params, headers);
   }
+
+  // post<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+  //   return this.http.post<T>(url, body, {params, headers})
+  // }
 
   put<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
-    return this.http.put<T>(url, body, {params, headers})
+    return this.request<T>('PUT', url, body, params, headers);
   }
+
+  // put<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+  //   return this.http.put<T>(url, body, {params, headers})
+  // }
 
   patch<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
-    return this.http.patch<T>(url, body, {params, headers})
+    return this.request<T>('PATCH', url, body, params, headers);
   }
 
+  // patch<T>(url: string, body: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+  //   return this.http.patch<T>(url, body, {params, headers})
+  // }
+
   delete<T>(url: string, body?: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
-    return this.http.delete<T>(url, {body, params, headers})
+    return this.request<T>('DELETE', url, body, params, headers);
   }
+
+  // delete<T>(url: string, body?: any, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+  //   return this.http.delete<T>(url, {body, params, headers})
+  // }
 
   toFormData<T>(formValue: T): FormData {
     const formData = new FormData()
@@ -54,6 +83,7 @@ export class HttpService {
   showNotificationHeader(): HttpHeaders {
     return new HttpHeaders().set(showNotificationHeader, 'true')
   }
+
   noCacheHeader(): HttpHeaders {
     return new HttpHeaders().set(noCacheHeader, 'true')
   }
